b5cb809d223b9b6e642474a3db33e8577f4e99e6
SetoYuhei <yuhei-seto@gmo.jp>
2017-05-25 15:08:15 +0900

    シャッフル実装

--- OldMaid/Card.java
+++ OldMaid/Card.java
@@ -1,8 +1,8 @@
 public class Card
 {
 	//カードの数字、スートを格納
-	private int number;
-	private int suit;
+	public int number;
+	public int suit;
 
 	//カード生成のための変数
 	public static int JOKER = 0;
@@ -17,7 +17,7 @@ public class Card
 	}
 	
 	//数字を見る
-	public int showNumber(){
+	public int showNumber(Card card){
 		return this.number;	
 	}
 }
